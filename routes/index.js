542d67bcfba4032ca0885c18cb66c07f69eea52d
Rinan <jandrewschnell@gmail.com>
2023-06-08 21:32:17 +0200

    iteration 1 done-ish

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+const User = require('../models/User.model');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.post("/", (req, res, next) => {
+  const hashpassword = bcrypt.hashSync(req.body.password, salt);
+  User.create({
+    username: req.body.username,
+    password: hashpassword,
+  })
+  .then(() => res.redirect(`/profile/${req.body.username}`))
+});
+
+router.get("/profile/:username", (req, res, next) => {
+  User.findOne({username: req.params.username})
+  .then((item)=> res.render("profile", item))
 });
 
 module.exports = router;
